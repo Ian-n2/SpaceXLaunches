e03d6790e84b505570ca1aab185188fe115ce9ef
Ian Neilson <ian@MacBook-Pro.local>
2020-12-09 14:59:03 +0000

    api working

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props){
+    super(props)
+    this.state = {
+      data: []
+    }
+  }
+
+  componentDidMount(){
+    fetch('https://api.spacexdata.com/v3/launches')
+    .then(res => res.json())
+    .then(data => this.setState({data: data}))
+
+  }
+
+  render(){
+    console.log(this.state.data)
+
+    return(
+      <div>
+      <h1>welcome</h1>
+      <ul>
+      {this.state.data.map(data => (
+        <li key={data.id}>
+        {data.mission_name}
+        </li>
+      ))}
+      </ul>
+      </div>
+    );
+  }
 }
 
 export default App;
